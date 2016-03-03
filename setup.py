dfe7967fdde4d8989501167dac9140a6514da5df
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2016-03-03 18:47:31 +0200

    Versiyon guncellendi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='Gonullu',
-      version='0.1',
+      version='0.3',
       description='Pisi Linux gonullu paket derleme uygulamasi',
       url='#',
       author='Ilker Manap',
