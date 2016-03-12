f000e75967c6cf34232187a4f5374982b0136414
Muhammet Dilmaç <iletisim@muhammetdilmac.com.tr>
2016-03-12 14:27:19 +0200

    setup adding clean command

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
-from setuptools import setup
+import os
+from setuptools import setup, Command
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
 
 setup(name='Gonullu',
       version='0.4',
